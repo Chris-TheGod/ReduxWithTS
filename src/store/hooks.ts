37a217b8b9aaedea8f626a87cd1b05e1a7a069d1
Kristjan Lääne <xonproject@gmail.com>
2024-04-25 18:28:51 +0300

    Dispatching Actions & Adjusting the useDispatch Hook

---
+++ src/store/hooks.ts
@@ -0,0 +1,6 @@
+import { UseDispatch, useDispatch } from 'react-redux';
+import { AppDispatch } from './store';
+
+type DispatchFunction = () => AppDispatch;
+
+export const useCartDispatch: DispatchFunction = useDispatch;
